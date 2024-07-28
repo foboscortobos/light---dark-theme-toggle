863adcda7d269b9ce205342922f30759b89fa8e4
foboscortobos <153738125+foboscortobos@users.noreply.github.com>
2024-07-28 11:53:40 +0200

    Update main.js

--- light-&-dark-theme-toggle/main.js
+++ light-&-dark-theme-toggle/main.js
@@ -3,3 +3,52 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
 });
+.link {
+  position: relative;
+  color: #fff;
+  text-transform: capitalize;
+  font-size: 20px;
+  padding: 10px;
+  transition: 0.5s;
+  letter-spacing: 2px;
+  list-style: none;
+  font-weight: 600;
+  cursor: pointer;
+}
+.link {
+  position: relative;
+  color: #fff;
+  text-transform: capitalize;
+  font-size: 20px;
+  padding: 10px;
+  transition: 0.5s;
+  letter-spacing: 2px;
+  list-style: none;
+  font-weight: 600;
+  cursor: pointer;
+}
+.link {
+  position: relative;
+  color: #fff;
+  text-transform: capitalize;
+  font-size: 20px;
+  padding: 10px;
+  transition: 0.5s;
+  letter-spacing: 2px;
+  list-style: none;
+  font-weight: 600;
+  cursor: pointer;
+}
+.link {
+  position: relative;
+  color: #fff;
+  text-transform: capitalize;
+  font-size: 20px;
+  padding: 10px;
+  transition: 0.5s;
+  letter-spacing: 2px;
+  list-style: none;
+  font-weight: 600;
+  cursor: pointer;
+}
+vvv
